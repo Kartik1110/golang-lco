d7a6d5119a13f2d71de1818176cee062ad1f123f
Kartik Pal <kartik.p1110@gmail.com>
2024-11-11 10:04:12 +0530

    feat: added slices

--- 09slices/main.go
+++ 09slices/main.go
@@ -1,8 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
 	fmt.Println("Slices in Go!")
 
+	var fruitsList = []string{"Apple", "Banana", "Cherry"}
+	fruitsList = append(fruitsList, "Dates")
+
+	fmt.Printf("Fruits list: %T\n", fruitsList)
+	fmt.Println("Fruits list:", fruitsList)
+
+	highScore := make([]int, 4)
+	highScore[0] = 234
+	highScore[1] = 945
+	highScore[2] = 465
+	highScore[3] = 867
+
+	highScore = append(highScore, 555) // append a new element to the slice
+
+	sort.Ints(highScore) // sort the slice in ascending order
+
+	fmt.Printf("High scores: %T\n", highScore)
+	fmt.Println("High scores:", highScore)
+
+	var courses = []string{"Python", "JavaScript", "Golang", "Rust", "Java"}
+	fmt.Println("Courses:", courses)
+
+	courses = append(courses[:2], courses[3])
+	fmt.Println("Courses:", courses)
 }
