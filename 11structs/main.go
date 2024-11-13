b37096ee40030f6c7b6b2e1314795b9eab3775ca
Kartik Pal <kartik.p1110@gmail.com>
2024-11-13 20:30:40 +0530

    feat: added structs

--- 11structs/main.go
+++ 11structs/main.go
@@ -5,4 +5,27 @@ import "fmt"
 func main() {
 	fmt.Println("Structs in Go!")
 
+	kartik := User{"Kartik", "kartik@gmail.com", true, 25}
+	fmt.Println(kartik)
+
+	fmt.Printf("Struct User: %+v\n", kartik)
+
+	broker := Broker{"Broker", "broker@gmail.com", true, 24, kartik}
+
+	fmt.Println("Broker:", broker)
+}
+
+type User struct {
+	Name   string
+	Email  string
+	Status bool
+	Age    int
+}
+
+type Broker struct {
+	Name     string
+	Email    string
+	Listings bool
+	Age      int
+	User     User
 }
