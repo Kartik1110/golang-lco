53782dc07c1bb0198e98811370b38ce4dee5bb85
Kartik Pal <kartik.p1110@gmail.com>
2024-11-13 21:28:31 +0530

    feat: added pointer

--- 07pointers/main.go
+++ 07pointers/main.go
@@ -6,7 +6,7 @@ func main() {
 	fmt.Println("Pointers in Go!")
 
 	var ptr *int // pointer to an integer
-	fmt.Println("Value of ptr: ", ptr)
+	fmt.Println("Value of ptr: ", ptr, &ptr)
 
 	myNumber := 23
 	var ptr1 = &myNumber // & means reference to the memory location of myNumber
@@ -14,4 +14,10 @@ func main() {
 
 	*ptr1 = *ptr1 + 10
 	fmt.Println("Value of myNumber: ", ptr1, *ptr1)
+
+	var myString *string
+	fmt.Println("Value of myString: ", myString, &myString)
+	str := "Hello"
+	myString = &str
+	fmt.Println("Value of myString after assignment: ", myString, *myString)
 }
